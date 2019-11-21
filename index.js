1f31f06233253c9cba7444de4bf8c50d2963b5ec
AndreaTosyCecconi <andrea-cecconi@hotmail.com>
2019-11-21 16:05:38 +0100

    update

--- index.js
+++ index.js
@@ -9,19 +9,31 @@ observer.observe(document.querySelector("body"), {
   subtree: true
 });
 
+
 function process() {
-  //logo
-  // const logo = document.querySelector(".rep-main-logo");
-  // if (!logo.classList.contains("logoRotated")) {
-  //   logo.classList.add("logoRotated");
-  // }
+
+  const imageDivs = document.querySelectorAll('.eLAPa');
+   for (const imageDiv of imageDivs) {
+    const image = imageDiv.querySelector(".FFVAD");
+    const text = image.getAttribute("alt");
+    const overlay = imageDiv.querySelector("._9AhH0");
+    if (!overlay.classList.contains("textProcessed")) {
+      overlay.classList.add("textProcessed");
+      overlay.textContent = text;
+
+    } 
+    }
+    
+  
 }
 
 // A message is received
 function receiver(request, sender, sendResponse) {
   if (request.active) {
+
     body.classList.add("my-beautiful-extension");
   } else {
+
     body.classList.remove("my-beautiful-extension");
   }
 }
